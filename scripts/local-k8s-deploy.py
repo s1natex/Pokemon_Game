6300013d20188ff349a71493d5980463fa3f7d6e
natan <your@email.here>
2025-10-16 17:05:44 +0300

    added rollout status to local-k8s-deploy.py to wait for admission webhook to be ready

--- scripts/local-k8s-deploy.py
+++ scripts/local-k8s-deploy.py
@@ -3,12 +3,18 @@ cmds = [
     ["kubectl","config","use-context","docker-desktop"],
     ["helm","repo","add","ingress-nginx","https://kubernetes.github.io/ingress-nginx"],
     ["helm","repo","update"],
-    ["helm","upgrade","--install","ingress-nginx","ingress-nginx/ingress-nginx","--namespace","ingress-nginx","--create-namespace"],
+    ["helm","upgrade","--install","ingress-nginx","ingress-nginx/ingress-nginx","-n","ingress-nginx","--create-namespace"],
+    ["kubectl","-n","ingress-nginx","rollout","status","deploy/ingress-nginx-controller","--timeout=180s"],
     ["kubectl","apply","-f","k8s/app/namespace.yaml"],
     ["kubectl","apply","-f","k8s/app/configmap.yaml"],
     ["kubectl","apply","-f","k8s/app/secret.yaml"],
     ["kubectl","apply","-f","k8s/app/rbac.yaml"],
-    ["kubectl","apply","-f","k8s/app/"]
+    ["kubectl","apply","-f","k8s/app/pokemon-manager.yaml"],
+    ["kubectl","apply","-f","k8s/app/trainer-manager.yaml"],
+    ["kubectl","apply","-f","k8s/app/battle-manager.yaml"],
+    ["kubectl","apply","-f","k8s/app/scheduler.yaml"],
+    ["kubectl","apply","-f","k8s/app/pokemon-fetcher.yaml"],
+    ["kubectl","apply","-f","k8s/app/frontend.yaml"]
 ]
 for c in cmds:
     run(c, check=True)
--- scripts/local-k8s-destroy.py
+++ scripts/local-k8s-destroy.py
@@ -1,10 +1,11 @@
 from subprocess import run
 cmds = [
     ["kubectl","config","use-context","docker-desktop"],
-    ["kubectl","delete","-f","k8s/app/"],
-    ["kubectl","delete","namespace","app"],
+    ["kubectl","delete","-f","k8s/app/frontend.yaml","--ignore-not-found=true"],
+    ["kubectl","delete","-f","k8s/app/","--ignore-not-found=true"],
+    ["kubectl","delete","namespace","app","--ignore-not-found=true"],
     ["helm","uninstall","ingress-nginx","-n","ingress-nginx"],
-    ["kubectl","delete","namespace","ingress-nginx"]
+    ["kubectl","delete","namespace","ingress-nginx","--ignore-not-found=true"]
 ]
 for c in cmds:
     run(c, check=True)
