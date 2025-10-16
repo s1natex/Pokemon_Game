f52cf6d9bace77e8bcfa0f9eaacb29d84d786900
natan <your@email.here>
2025-10-16 16:58:31 +0300

    updated local deploy destroy with ingress controller install so ingress can work locally

--- scripts/local-k8s-deploy.py
+++ scripts/local-k8s-deploy.py
@@ -1,6 +1,9 @@
 from subprocess import run
 cmds = [
     ["kubectl","config","use-context","docker-desktop"],
+    ["helm","repo","add","ingress-nginx","https://kubernetes.github.io/ingress-nginx"],
+    ["helm","repo","update"],
+    ["helm","upgrade","--install","ingress-nginx","ingress-nginx/ingress-nginx","--namespace","ingress-nginx","--create-namespace"],
     ["kubectl","apply","-f","k8s/app/namespace.yaml"],
     ["kubectl","apply","-f","k8s/app/configmap.yaml"],
     ["kubectl","apply","-f","k8s/app/secret.yaml"],
--- scripts/local-k8s-destroy.py
+++ scripts/local-k8s-destroy.py
@@ -2,7 +2,9 @@ from subprocess import run
 cmds = [
     ["kubectl","config","use-context","docker-desktop"],
     ["kubectl","delete","-f","k8s/app/"],
-    ["kubectl","delete","namespace","app"]
+    ["kubectl","delete","namespace","app"],
+    ["helm","uninstall","ingress-nginx","-n","ingress-nginx"],
+    ["kubectl","delete","namespace","ingress-nginx"]
 ]
 for c in cmds:
     run(c, check=True)
