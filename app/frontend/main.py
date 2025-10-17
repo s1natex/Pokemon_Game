d5aab77f89f414799c021a913b5e655b15355d4c
natan <your@email.here>
2025-10-18 00:23:00 +0300

    test ci frontend change

--- app/frontend/main.py
+++ app/frontend/main.py
@@ -22,7 +22,7 @@ def healthz():
 @app.get("/")
 def root():
     # body for root endpoint
-    return {"service": "frontend", "message": "pokemon mvp(test1)"}
+    return {"service": "frontend", "message": "pokemon mvp(test2)"}
 
 @app.get("/metrics")
 def metrics():
