a583244d623418dc9b2efc2fe54f32c45db0987d
natan <your@email.here>
2025-10-17 13:53:36 +0300

    updated eks deploy destroy scripts to also run validation commands

--- scripts/eks-k8s-deploy.py
+++ scripts/eks-k8s-deploy.py
@@ -1,4 +1,5 @@
 from subprocess import run
+
 cmds = [
     ["aws","eks","update-kubeconfig","--name","pokemon-game-eks","--region","eu-central-1","--alias","pokemon-game-eks"],
     ["kubectl","config","use-context","pokemon-game-eks"],
@@ -12,7 +13,16 @@ cmds = [
     ["kubectl","apply","-f","k8s/app/scheduler.yaml"],
     ["kubectl","apply","-f","k8s/app/pokemon-fetcher.yaml"],
     ["kubectl","apply","-f","k8s/app/frontend.yaml"],
-    ["kubectl","apply","-f","k8s/app/ingress-alb.yaml"]
+    ["kubectl","apply","-f","k8s/app/ingress-alb.yaml"],
+    ["kubectl", "-n", "app", "delete", "ingress", "app-ingress"],
+    ["echo", "==================================="],
+    ["echo", "Deployment completed successfully"],
+    ["echo", "==================================="],
+    ["sleep", "20"],
+    ["kubectl", "get", "pods", "-n", "app"],
+    ["kubectl", "get", "svc", "-n", "app"],
+    ["kubectl", "get", "ingress", "-n", "app"]
 ]
+
 for c in cmds:
     run(c, check=True)
--- scripts/eks-k8s-destroy.py
+++ scripts/eks-k8s-destroy.py
@@ -1,10 +1,14 @@
 from subprocess import run
+
 cmds = [
     ["aws","eks","update-kubeconfig","--name","pokemon-game-eks","--region","eu-central-1","--alias","pokemon-game-eks"],
     ["kubectl","config","use-context","pokemon-game-eks"],
     ["kubectl","delete","-f","k8s/app/ingress-alb.yaml","--ignore-not-found=true"],
     ["kubectl","delete","-f","k8s/app/","--ignore-not-found=true"],
-    ["kubectl","delete","namespace","app","--ignore-not-found=true"]
+    ["kubectl","delete","namespace","app","--ignore-not-found=true"],
+    ["kubectl", "get", "ns", "|", "grep", "app"],
+    ["kubectl", "get", "pods", "-A"]
 ]
+
 for c in cmds:
     run(c, check=True)
